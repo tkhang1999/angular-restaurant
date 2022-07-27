3721fe0eadb9e73deb030666f68a7a714e576f50
Khang Le <tkhang1999@gmail.com>
2022-07-27 14:22:19 -0700

    add a basic dish service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,7 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
