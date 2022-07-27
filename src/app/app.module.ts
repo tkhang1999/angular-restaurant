2184213bb9930019611c7b1ee66e1c44ed5ad55a
Khang Le <tkhang1999@gmail.com>
2022-07-26 22:02:54 -0700

    implement dish detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    DishDetailComponent,
   ],
   imports: [
     BrowserModule,
