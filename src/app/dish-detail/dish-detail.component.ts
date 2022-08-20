baec4484cfd84349eedd47cf25c78cb4bd0b3e23
Khang Le <tkhang1999@gmail.com>
2022-08-20 10:22:57 +0800

    add animation

--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -6,11 +6,37 @@ import { Dish } from '../shared/dish';
 import { switchMap, tap } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-dish-detail',
   templateUrl: './dish-detail.component.html',
   styleUrls: ['./dish-detail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state(
+        'shown',
+        style({
+          transform: 'scale(1.0)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          transform: 'scale(0.5)',
+          opacity: 0,
+        })
+      ),
+      transition('* => *', animate('0.5s ease-in-out')),
+    ]),
+  ],
 })
 export class DishDetailComponent implements OnInit {
   dish?: Dish;
@@ -19,6 +45,7 @@ export class DishDetailComponent implements OnInit {
   next?: string;
   errMess?: string;
   dishCopy?: Dish;
+  visibility = 'shown';
 
   commentForm: FormGroup;
   @ViewChild('cform') commentFormDirective: any;
@@ -73,14 +100,19 @@ export class DishDetailComponent implements OnInit {
     this.route.params
       .pipe(
         // set dish to undefined for loading side effect while waiting for dish service
-        tap(() => (this.dish = undefined)),
-        switchMap((params: Params) => this.dishService.getDish(params['id']))
+        tap(() => {
+          this.visibility = 'hidden';
+        }),
+        switchMap((params: Params) => {
+          return this.dishService.getDish(params['id']);
+        })
       )
       .subscribe({
         next: (dish) => {
           this.dish = dish;
           this.dishCopy = dish;
           this.setPrevNext(dish.id);
+          this.visibility = 'shown';
         },
         error: (errMess) => (this.errMess = errMess),
       });
